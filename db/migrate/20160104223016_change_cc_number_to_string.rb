eb0eeb2e3ed60ffe76fdd37018e6e5e5bcac4707
Sekhar Paladugu <sekharp@gmail.com>
2016-01-04 15:53:58 -0700

    csv data import

---
+++ db/migrate/20160104223016_change_cc_number_to_string.rb
@@ -0,0 +1,5 @@
+class ChangeCcNumberToString < ActiveRecord::Migration
+  def change
+    change_column :transactions, :credit_card_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160104212745) do
+ActiveRecord::Schema.define(version: 20160104223016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20160104212745) do
 
   create_table "transactions", force: :cascade do |t|
     t.integer  "invoice_id"
-    t.integer  "credit_card_number"
+    t.string   "credit_card_number"
     t.date     "credit_card_expiration_date"
     t.string   "result"
     t.datetime "created_at",                  null: false
